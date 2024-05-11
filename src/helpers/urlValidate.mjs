78dd729f6ff8577a3946fe8c87ffbbe10207e498
Crno0 <123456789+odin@users.noreply.github.com>
2024-05-11 17:21:30 +0800

    Create a url validator function

---
+++ src/helpers/urlValidate.mjs
@@ -0,0 +1,9 @@
+const validateUrl = (string) => {
+  // https://regexr.com/806jh
+  const regex =
+    /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/;
+
+  return regex.test(string);
+};
+
+export default validateUrl;
