589758883c7787988c0fd2bca1aecd845ff249fa
Crno0 <123456789+odin@users.noreply.github.com>
2024-05-13 17:50:13 +0800

    fix(models): change array of possessors to string

--- src/models/sefirah.mjs
+++ src/models/sefirah.mjs
@@ -1,24 +1,22 @@
-import mongoose from "mongoose";
-import validateUrl from "../helpers/urlValidate.mjs";
+import mongoose from 'mongoose';
+import validateUrl from '../helpers/urlValidate.mjs';
 
 const Schema = mongoose.Schema;
 
 const sefirahSchema = new Schema({
-  name: { type: String, minLength: 3, maxLength: 100, required: true },
-  image: {
-    url: {
-      type: String,
-      validate: {
-        validator: validateUrl,
-      },
-      message: (props) => `${props.value} is not a valid URL`,
+    name: { type: String, minLength: 3, maxLength: 100, required: true },
+    image: {
+        url: {
+            type: String,
+            validate: {
+                validator: validateUrl,
+            },
+            message: (props) => `${props.value} is not a valid URL`,
+        },
+        cloudinary_id: { type: String },
+        possessor: { type: String, minLength: 3, maxLength: 100},
+        description: { type: String, minLength: 3, required: true },
     },
-    cloudinary_id: { type: String },
-    possessors: [
-      { type: String, minLength: 3, maxLength: 100, required: true },
-    ],
-    description: { type: String, minLength: 3, required: true },
-  },
 });
 
-export default mongoose.model("Sefirah", sefirahSchema);
+export default mongoose.model('Sefirah', sefirahSchema);
