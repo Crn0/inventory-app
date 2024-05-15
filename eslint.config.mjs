ac061c967bd62c47696a431c4e92a4506a088c80
Crno0 <123456789+odin@users.noreply.github.com>
2024-05-15 18:07:26 +0800

    style: format code

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,27 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 export default [
-  pluginJs.configs.recommended,
-  {
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      globals: globals.node,
-    },
-  },
-  {
-    rules: {
-      "func-names": "off",
-      "prefer-arrow-callback": "warn",
-      "no-unused-vars": [
-        "error",
-        {
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
+    pluginJs.configs.recommended,
+    {
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: globals.node,
         },
-      ],
-      "no-undef": "error",
     },
-  },
+    {
+        rules: {
+            'func-names': 'off',
+            'prefer-arrow-callback': 'warn',
+            'no-unused-vars': [
+                'error',
+                {
+                    argsIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                },
+            ],
+            'no-undef': 'error',
+        },
+    },
 ];
