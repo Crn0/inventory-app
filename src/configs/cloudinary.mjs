0ab71cde30a2138eb9a15e1c6d3bfac803244cd6
Crno0 <123456789+odin@users.noreply.github.com>
2024-05-15 18:05:37 +0800

    feat(config): create cloudinary config and upload func

---
+++ src/configs/cloudinary.mjs
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+import { v2 as cloudinary } from 'cloudinary';
+
+// Configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View Credentials' below to copy your API secret
+});
+
+const UploadImage = async (url, tags) => {
+    try {
+        const res = await cloudinary.uploader.upload(url, { tags });
+
+        return res;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+// UploadImage(`/home/ivan/repos/inventory-app/public/images/Fool_Art.png`)
+
+export default UploadImage;
