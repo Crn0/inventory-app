838ecb6a6d52cb82ba73f765bed29e4d915f1a31
Crno0 <123456789+odin@users.noreply.github.com>
2024-05-29 19:28:14 +0800

    feat(image_api): create a cloudinary class
    Create a method to upload image
    Create a method to update image
    Create a method to destroy image

---
+++ src/helpers/uploadAndUpdateImage.mjs
@@ -0,0 +1,41 @@
+import { cloudinary } from "../configs/cloudinary.mjs";
+
+class Cloudinary {
+    constructor () {}
+
+    static async upload(url, tags) {
+        try {
+            const res = await cloudinary.uploader.upload(url, { tags });
+
+            return res;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    static async update(url, public_id, invalidate = true) {
+        try {
+            const res = await cloudinary.uploader.upload(url,  {
+                    public_id,
+                    invalidate 
+                }
+        );
+
+            return res;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    static async destroy(public_id, invalidate = true) {
+        try {
+            const res = await cloudinary.uploader.destroy(public_id, { invalidate });
+
+            return res;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
+
+export default Cloudinary
